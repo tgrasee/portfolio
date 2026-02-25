9834f4066c0b066365c2c4de8c8117ed5534f494
Tyler <87778277+tgrasee@users.noreply.github.com>
2026-02-25 11:19:43 -0500

    add connectedCallback

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ class Header extends HTMLElement {
   constructor() {
     super();
   }
+  
+  connectedCallback() {
+    this.innerHTML = `
+      <h3><a href="/portfolio/index.html">Tyler Grasee</a></h3>
+      <ul>
+        <li><a href="">Google Map API</a></li>
+        <li><a href="/portfolio/projects/etchasketch/etchasketch.html">Etch-a-Sketch</a></li>
+        <li><a href="/portfolio/projects/rockpaperscissors/rps.html">RPS Game</a></li>       
+      </ul>
+    `;
+  }
 }
 
 customElements.define('header-component', Header, { extends: 'header' });
