ce31b476eb0c5d8e229be5c2b0a8a9c858ade960
Tyler <87778277+tgrasee@users.noreply.github.com>
2026-02-25 12:21:52 -0500

    add style to shadowdom of header-component

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ class Header extends HTMLElement {
     this.innerHTML = `
         <header>
             <h3><a href="/portfolio/index.html">Tyler Grasee</a></h3>
-            <ul>
-                <li><a href="">Google Map API</a></li>
-                <li><a href="/portfolio/projects/etchasketch/etchasketch.html">Etch-a-Sketch</a></li>
-                <li><a href="/portfolio/projects/rockpaperscissors/rps.html">RPS Game</a></li>       
-            </ul>
+            <nav>
+                <ul>
+                    <li><a href="">Google Map API</a></li>
+                    <li><a href="/portfolio/projects/etchasketch/etchasketch.html">Etch-a-Sketch</a></li>
+                    <li><a href="/portfolio/projects/rockpaperscissors/rps.html">RPS Game</a></li>       
+                </ul>
+            </nav>
         </header>
     `;
   }
