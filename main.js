9b4adfe9e6d764d6caaf6295c8febbd8b7c96bf5
Tyler <87778277+tgrasee@users.noreply.github.com>
2026-02-25 13:52:41 -0500

    comment out style in headerTemplate

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 const headerTemplate = document.createElement('template');
 
 headerTemplate.innerHTML = `
-    <style>
-        header {
-            background-color: #1F2937;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 0 48px 0;
-            color: white;
-        }
+    // <style>
+    //     header {
+    //         background-color: #1F2937;
+    //         display: flex;
+    //         justify-content: space-between;
+    //         align-items: center;
+    //         padding: 0 48px 0;
+    //         color: white;
+    //     }
 
-        ul {
-            display: flex;
-            list-style: none;
-            gap: 16px;
-            color: white;
-        }
+    //     ul {
+    //         display: flex;
+    //         list-style: none;
+    //         gap: 16px;
+    //         color: white;
+    //     }
 
-        a {
-            text-decoration: none;
-            color: #e5e7eb;
-        }
-    </style>
+    //     a {
+    //         text-decoration: none;
+    //         color: #e5e7eb;
+    //     }
+    // </style>
     <header>
         <h3><a href="/portfolio/index.html">Tyler Grasee</a></h3>
         <nav>
