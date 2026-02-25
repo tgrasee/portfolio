cc31756bb201edbc8cee9168d59d109dc35dfb5e
Tyler <87778277+tgrasee@users.noreply.github.com>
2026-02-25 12:31:40 -0500

    add style to shadowdom of header-component

--- main.js
+++ main.js
@@ -5,6 +5,28 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
+        <style>
+            header {
+                background-color: #1F2937;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 0 48px 0;
+                color: white;
+            }
+
+            ul {
+                display: flex;
+                list-style: none;
+                gap: 16px;
+                color: white;
+            }
+
+            a {
+                text-decoration: none;
+                color: #e5e7eb;
+            }
+        </style>
         <header>
             <h3><a href="/portfolio/index.html">Tyler Grasee</a></h3>
             <nav>
