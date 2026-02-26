a4207672bb8522beb2b5f82c241a13960c5f3772
Tyler <87778277+tgrasee@users.noreply.github.com>
2026-02-26 14:05:43 -0500

    update google map api link

--- components/header-component.js
+++ components/header-component.js
@@ -44,7 +44,7 @@ headerTemplate.innerHTML = `
         <h3><a href="/portfolio/index.html">Tyler Grasee</a></h3>
         <nav>
             <ul>
-                <li><a href="">Google Map API</a></li>
+                <li><a href="/portfolio/projects/googlemapapi/googleapi.html">Google Map API</a></li>
                 <li><a href="/portfolio/projects/etchasketch/etchasketch.html">Etch-a-Sketch</a></li>
                 <li><a href="/portfolio/projects/rockpaperscissors/rps.html">RPS Game</a></li>       
             </ul>
