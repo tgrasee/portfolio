0ed2f54dd124b7a511429763367ab7830277f7d7
Tyler <87778277+tgrasee@users.noreply.github.com>
2026-02-28 09:01:55 -0500

    initial commit

---
+++ components/footer-component.js
@@ -0,0 +1,59 @@
+const footerTemplate = document.createElement('template');
+
+// <style>
+    //     header {
+    //         background-color: #1F2937;
+    //         display: flex;
+    //         justify-content: space-between;
+    //         align-items: center;
+    //         padding: 0 48px 0;
+    //         color: white;
+    //     }
+
+    //     ul {
+    //         display: flex;
+    //         list-style: none;
+    //         gap: 16px;
+    //         color: white;
+    //     }
+
+    //     a {
+    //         text-decoration: none;
+    //         color: #e5e7eb;
+    //     }
+    // </style>
+
+footerTemplate.innerHTML = `
+    <style>
+        ul {
+            display: flex;
+            list-style: none;
+            gap: 16px;
+            color: white;
+        }
+
+        a {
+            text-decoration: none;
+            color: #e5e7eb;
+        }
+
+        a:hover {
+            color: #9ca3af;
+        }
+    </style>
+        <h3>Copyright © 2026 Tyler Grasee</h3>
+`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+    shadowRoot.appendChild(footerTemplate.content);
+  }
+}
+
+customElements.define('footer-component', Footer);
