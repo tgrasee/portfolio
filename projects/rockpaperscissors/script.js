572812697a704b077cdd62962a82e407589d45e8
Tyler <87778277+tgrasee@users.noreply.github.com>
2026-02-28 12:16:32 -0500

    update main selector to game

--- projects/rockpaperscissors/script.js
+++ projects/rockpaperscissors/script.js
@@ -5,7 +5,7 @@ const playerScoreBoard = document.querySelector(".playerCount");
 const computerScoreBoard = document.querySelector(".computerCount");
 const result = document.querySelector(".results");
 const buttons = document.querySelector(".buttons");
-const mainElement = document.querySelector("main");
+const gameElement = document.querySelector(".game");
 const restartContainer = document.createElement("div");
 const restart = document.createElement("button");
 restartContainer.className = "restartContainer";
@@ -78,7 +78,7 @@ function gameOver() {
 
   // Add restart button
   restartContainer.append(restart);
-  mainElement.appendChild(restartContainer);
+  gameElement.appendChild(restartContainer);
   restart.textContent = "Restart";
   restart.addEventListener("click", () => {
     window.location.reload();
